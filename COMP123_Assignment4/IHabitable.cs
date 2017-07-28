054f56dfd37dbec4a79ab856ef5d862f9e1d128a
300921431 <mika.mikangkim@gmail.com>
2017-07-28 18:18:38 -0400

    Set up all Interface

---
+++ COMP123_Assignment4/IHabitable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Student ID: 300921431
+ Student Name: Mikang Kim 
+ This is interface which is for Terrestrial Planet
+     */
+
+namespace COMP123_Assignment4
+{
+    interface IHabitable
+    {
+        bool Habitable();
+    }
+}
