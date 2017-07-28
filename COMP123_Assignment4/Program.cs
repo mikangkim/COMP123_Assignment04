074bd2457fcb83be7fdf1b3f2ce373d3a86d90ac
300921431 <mika.mikangkim@gmail.com>
2017-07-28 18:04:57 -0400

    Build main class

--- COMP123_Assignment4/Program.cs
+++ COMP123_Assignment4/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ Student ID: 300921431
+ Student Name: Mikang Kim 
+ This is Main method of my driver class
+     */
+
 namespace COMP123_Assignment4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GiantPlanet giantPlanet = new GiantPlanet;
+            Console.WriteLine();
+
+            TerrestrialPlanet TerrestrialPlanet = new TerrestrialPlanet;
+            Console.WriteLine();
+
+            WaitForAnyKey();
+        }
+
+
+        private static void WaitForAnyKey()
+        {
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
         }
     }
 }
+
